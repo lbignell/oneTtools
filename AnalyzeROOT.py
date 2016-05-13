a6cc00937244351705aef6711c3a27437fd7ae41
lbignell <lindsey.j.bignell@gmail.com>
2016-05-13 11:42:48 -0400

    SaveCanvas, legend option added

--- AnalyzeROOT.py
+++ AnalyzeROOT.py
@@ -10,6 +10,9 @@ import ROOT
 import matplotlib.pyplot as plt
 
 def GetHists(rootfile, prefix, chlist, colorlist=None):
+    '''
+    Get a list of hists with name format prefix+chlist
+    '''
     hists = []
     if colorlist is None:
         colorlist = [i+1 for i in range(len(chlist))]
@@ -18,6 +21,14 @@ def GetHists(rootfile, prefix, chlist, colorlist=None):
         thehist.SetLineColor(colorlist[i])
         hists.append(thehist)
     return hists
+
+def SaveCanvas(canvas, filepath, basename, formats=['.svg', '.png', '.C']):
+    '''
+    Save a ROOT canvas in filepath with name basename.
+    '''
+    for ext in formats:
+        canvas.SaveAs(filepath+basename+ext)
+    return
 
 def plotHists(histlist, xlabel="", ylabel="", newcanvas=True,
               legtitle="", leglabels=None):
@@ -31,12 +42,24 @@ def plotHists(histlist, xlabel="", ylabel="", newcanvas=True,
             hist.Draw("")
             hist.SetStats(0)
             hist.GetXaxis().SetTitle(xlabel)
+            hist.GetXaxis().SetTitleSize(0.05)
             hist.GetYaxis().SetTitle(ylabel)
+            hist.GetYaxis().SetTitleSize(0.05)
+            firsthist = hist
             first = False
         else:
             hist.Draw("SAME")
+    leg = ROOT.TLegend(0.1, 0.7, 0.3, 0.9)
+    if legtitle:
+        leg.SetHeader(legtitle)
+    if len(histlist) == len(leglabels):
+        [leg.AddEntry(histlist[i], leglabels[i]) for i in range(len(histlist))]
+        leg.Draw()
+    thecanvas.Update()
     thetitle = thecanvas.FindObject('title')
     thetitle.Clear()
+    thecanvas.Update()
+    return histlist, firsthist, leg
     
 def plot2DbyChannel(thehist, normed=True, chlist=[], newfig=True, xlim=False):
     numchans = thehist.GetNbinsY()
